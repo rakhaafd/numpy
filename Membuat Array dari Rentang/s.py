902837ed0b9b19adc0af1c69f7053dcd98406167
rakhaafd <rakhafausta07@gmail.com>
2025-01-18 18:48:53 +0700

    update all

--- Membuat Array dari Rentang/s.py
+++
@@ -1,6 +0,0 @@
-import numpy
-
-# numpy arange(): buat array berdasar range
-#numpy.arrange(mulai, akhir, step, dtype)
-my_arr = numpy.arange(10)
-print(my_arr)
---
+++ membuat_array_dari_rentang.py
@@ -0,0 +1,10 @@
+import numpy as kntl
+
+# numpy arange(): buat array berdasar range
+#numpy.arrange(mulai, akhir, step, dtype)
+# my_arr = kntl.arange(1, 11, 2, dtype=str)
+# print(my_arr)
+
+#linspace(mulai, akhir, elemen, endpoint, retstep, dtype)
+my_arr2 = kntl.linspace(0, 10, 5)
+print(my_arr2)
